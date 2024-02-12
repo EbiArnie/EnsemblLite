d5c5fe6097c25cbb55198356db4b9888c3325c0e
Arne Becker <arne@ebi.ac.uk>
2024-02-12 23:01:02 +0000

    ENH: Fetch repeats and write as parquet
    - dd_fetch_repeats will now copy over the schema for any given species,
      then fetch the corresponding data files from FTP.
      The data is read into duckdb, denormalized and filtered into a single
      table which is then written out to a compressed parquet file.

--- experiments/dd_fetch_repeats.py
+++ experiments/dd_fetch_repeats.py
@@ -4,87 +4,114 @@ import duckdb
 from tempfile import TemporaryDirectory
 from tqdm import tqdm
 import requests
+import os
 
 # python -m venv venv
 # . venv/bin/activate
 # pip install duckdb tqdm requests
 
-# Using public MySQL DBs:
+# The duckdb mysql plugin only seems to work with the ensembldb MySQL servers
 DBHOST = "ensembldb.ensembl.org"
-# DBHOST = "asiadb.ensembl.org"
-# DBHOST = "useastdb.ensembl.org"
-
 DBPORT = 3306
 DBUSER = "anonymous"
 
-DBNAME = "homo_sapiens_core_111_38"
 # DBNAME = "bison_bison_bison_core_111_1"
-# DBNAME = "saccharomyces_cerevisiae_core_111_4"
+SPECIES_DBS = ["saccharomyces_cerevisiae_core_111_4",
+               "homo_sapiens_core_111_38",
+               "bison_bison_bison_core_111_1"]
 
+# This experiment uses release 111.
+FTP_DIR = f"https://ftp.ensembl.org/pub/release-111/mysql/"
+# local directory name for data output
+LOCAL_DIR="data"
 
-# This experiment uses the default homo sapiens, release 111.
-FTP_DIR = f"https://ftp.ensembl.org/pub/release-111/mysql/{DBNAME}/"
-
-IMPORT_TABLES = [
+REPEAT_TABLES = [
     "seq_region",
     "repeat_feature",
     "repeat_consensus",
-    "analysis",
-    "meta",
-    "coord_system"
 ]
 
 # Open file-based DB, would be in-memory without a file name
-CON = duckdb.connect('experiment-repeat.db')
+#CON = duckdb.connect('experiment-repeat.db')
+CON = duckdb.connect()
 
 
 def main():
     print("Preparing a DuckDB in './experiment-repeat.db' for repeat features "
-          f"for {DBNAME}, EnsEMBL release 111")
-
-    clean_db()
-    read_db_tables()
-    print("Table definition import OK")
-    print("Fetching dump files")
-
-    # will be removed at end of main
-    tmpdir = TemporaryDirectory(prefix="dd_fetch_repeats")
-    fetch_tsv_dumps(tmpdir)
-
-    print("Fetch OK")
-
-    ingest_dumps(tmpdir)
-
-    print("Done")
+          f"for EnsEMBL release 111")
 
 
-def read_db_tables():
+    for dbname in SPECIES_DBS:
+        path = f"{LOCAL_DIR}/{dbname}/"
+        if os.path.exists(path):
+            print(f"Found local data for: {path}")
+            continue
+
+        clean_db()
+        read_db_tables(dbname)
+        print("Table definition import OK, fetching dump files")
+
+        # will be removed at end of main
+        tmpdir = TemporaryDirectory(prefix="dd_fetch_repeats")
+        fetch_tsv_dumps(dbname, tmpdir)
+
+        print("Fetch OK")
+
+        ingest_dumps(tmpdir)
+        filter_and_write_data(dbname)
+
+    for dbname in SPECIES_DBS:
+        print()
+        print(f"Statistics for: {dbname}")
+        outfile = f"{LOCAL_DIR}/{dbname}/repeats.parquet"
+        sql = f"SUMMARIZE FROM read_parquet('{outfile}')"
+        res = sql_ex(sql)
+        print(res.fetchnumpy())
+
+        print("Done")
+
+
+def filter_and_write_data(dbname):
+    path = f"{LOCAL_DIR}/{dbname}/"
+    os.makedirs(path)
+    outfile = f"{LOCAL_DIR}/{dbname}/repeats.parquet"
+    sql = ("""
+        COPY (
+            SELECT
+                name, repeat_start, repeat_end,
+                CASE WHEN seq_region_strand = true THEN 1 ELSE -1 END as strand,
+                repeat_type, repeat_class, score
+            FROM repeat_feature
+            INNER JOIN repeat_consensus USING (repeat_consensus_id)
+            INNER JOIN seq_region USING (seq_region_id)
+        """
+        f") TO '{outfile}' (COMPRESSION ZSTD);"
+    )
+    sql_ex(sql)
+
+
+
+def read_db_tables(dbname):
     sql = "INSTALL mysql"
-    print(sql)
     sql_ex(sql)
 
     sql = "LOAD mysql"
-    print(sql)
     sql_ex(sql)
 
-    sql = f"ATTACH 'host={DBHOST} port={DBPORT} user={DBUSER} database={DBNAME}' AS mysqldb (TYPE mysql)"
-    print(sql)
+    sql = f"ATTACH 'host={DBHOST} port={DBPORT} user={DBUSER} database={dbname}' AS mysqldb (TYPE mysql)"
     sql_ex(sql)
 
-    for tbl in IMPORT_TABLES:
+    for tbl in REPEAT_TABLES:
         sql = f"create table {tbl} as select * from mysqldb.{tbl} limit 0"
-        print(sql)
         sql_ex(sql)
 
     sql = "DETACH mysqldb"
-    print(sql)
     sql_ex(sql)
 
 
-def fetch_tsv_dumps(tmpdir):
-    url = FTP_DIR
-    for tbl in IMPORT_TABLES:
-        url = f"{FTP_DIR}{tbl}.txt.gz"
+def fetch_tsv_dumps(dbname, tmpdir):
+    for tbl in REPEAT_TABLES:
+        url = f"{FTP_DIR}/{dbname}/{tbl}.txt.gz"
         filepath = f"{tmpdir.name}/{tbl}.txt.gz"
 
         print(f"Fetching {url}, writing to {filepath}")
@@ -106,7 +133,7 @@ def fetch_tsv_dumps(tmpdir):
 
 
 def ingest_dumps(tmpdir):
-    for tbl in IMPORT_TABLES:
+    for tbl in REPEAT_TABLES:
         filepath = f"{tmpdir.name}/{tbl}.txt.gz"
         print(f"Ingest for table {tbl}")
 
@@ -115,20 +142,17 @@ def ingest_dumps(tmpdir):
 
 
 def clean_db():
-    for tbl in IMPORT_TABLES:
+    for tbl in REPEAT_TABLES:
         sql = f"DROP TABLE IF EXISTS {tbl}"
         sql_ex(sql)
 
 
-def db_size():
-    sql = "PRAGMA database_size"
-    print(sql_fetchone(sql))
-
-
 def sql_ex(sql):
+    print(sql)
     res = CON.execute(sql)
     if res == None:
         raise (Exception("DuckDB error: No result for: {sql}"))
+    return res
 
 
 def sql_fetchone(sql):
@@ -136,6 +160,20 @@ def sql_fetchone(sql):
     if res == None:
         raise (Exception("DuckDB error: No result for: {sql}"))
     return res
+
+
+# Write out complete DB, data and schema
+# def write_parquet(dbname):
+#     path = f"{LOCAL_DIR}/{dbname}/"
+#     os.makedirs(path)
+#     sql = f"EXPORT DATABASE '{path}' (FORMAT PARQUET)"
+#     sql_ex(sql)
+
+
+# Show DB size
+#def db_size():
+#    sql = "PRAGMA database_size"
+#    print(sql_fetchone(sql))
 
 
 main()
